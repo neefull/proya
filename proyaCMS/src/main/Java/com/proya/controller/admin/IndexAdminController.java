64725573f66b9aaeae6a8ef82eca4b8c2ea3b858
wuzhifeng <839300274@qq.com>
2017-05-23 23:00:20 +0800

    环境搭建

---
+++ proyaCMS/src/main/Java/com/proya/controller/admin/IndexAdminController.java
@@ -0,0 +1,10 @@
+package com.proya.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by wzf on 2017/5/23.
+ */
+@Controller
+public class IndexAdminController {
+}
