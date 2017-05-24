c722d40fe5009fe1208e163db6c97f9819890e2d
wuzhifeng <839300274@qq.com>
2017-05-25 00:11:43 +0800

    springmvc配置

--- proyaCMS/src/main/Java/com/proya/controller/admin/IndexAdminController.java
+++ proyaCMS/src/main/Java/com/proya/controller/admin/IndexAdminController.java
@@ -1,10 +1,19 @@
 package com.proya.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by wzf on 2017/5/23.
  */
 @Controller
+@RequestMapping("indexAdmin")
 public class IndexAdminController {
+
+    @RequestMapping("/helloworld")
+    public String hello(){
+        System.out.println("hello world");
+        return "index";
+    }
+
 }
---
+++ proyaCMS/src/main/Java/com/proya/service/admin/indexService.java
@@ -0,0 +1,10 @@
+package com.proya.service.admin;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by wzf on 2017/5/24.
+ */
+@Service
+public class indexService {
+}
